115bd4696560a45d6ccf0e146ff9a8fd1d3b0d6e
CrimsonTorso <40295915+CrimsonTorso@users.noreply.github.com>
2019-03-23 13:46:30 +0000

    Update Grappler.py

--- Grappler.py
+++ Grappler.py
@@ -4,5 +4,7 @@ import core, os, time
 
 core.welcome_banner()
 core.author()
+time.sleep(1.5)
 user_input = raw_input("Please enter a search query: ")
 responce = os.system("grep -i " + user_input + " *")
+time.sleep(2.5)
